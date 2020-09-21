23e0ff65e4b8eb3b3024f48eb73a84cdf4ff633b
suthyscott <sutherlandscott21@gmail.com>
2020-09-21 08:59:34 -0600

    working as a class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class StateApp extends React.Component {
+
+    constructor(){
+        super()
+
+        this.state = {
+            counrtyId: 0,
+            patriotic: false,
+            search: '',
+            tanks: []
+        }
+    }
+
+    componentDidMount(){
+        this.getTanks()
+    }
+
+
+     getTanks = () => {
+        const {patriotic, search, countryId} = this.state
+        // patriotic just dictates whether or not you want to see your country's tanks
+        axios.get(`/api/tanks/${countryId}?search=${search}&patriotic=${patriotic}`)
+          .then(res => {
+            this.setState({
+                tanks: res.data
+            })
+          })
+          .catch(err => console.log(err))
+      }
+    
+      handleInput = e =>{
+          this.setState({
+            [e.target.name]: e.target.value
+          })
+      }
+
+      togglePatriotic = () => {
+          this.setState({
+              patriotic: !this.state.patriotic
+          })
+      }
+   
+      render(){
+        const {patriotic, search, countryId, tanks} = this.state
+        return (
+            <div className="App">
+                <input placeholder='search' value={search} name='search' onChange={(e) => this.handleInput(e)}/>
+                <input type='number' placeholder='Country ID' value={countryId} name='countryId' onChange={(e) => this.handleInput(e)}/>
+                <input type='checkbox' placeholder='Patriotic?' name='patriotic' onChange={(e) => this.togglePatriotic()}/>
+                <button onClick={() => this.getTanks()}>Filter</button>
+                {tanks.map(tank => {
+                  return (
+                    <div key={tank.tank_id}>
+                      {tank.tank_name}
+                    </div>
+                  )
+                })}
+            </div>
+          );
+      }
 }
 
-export default App;
+export default StateApp
